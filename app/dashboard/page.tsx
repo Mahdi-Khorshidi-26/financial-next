1813e66e4579748be7df669240d837e69f5631c0
Mahdi-Khorshidi-26 <khorshidi.mahdi.1998@gmail.com>
2025-09-13 01:30:07 +0330

    installing the error boundary .

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,17 +4,26 @@ import TransactionListFallback from "./components/transactionListFallback";
 import Link from "next/link";
 import { PlusCircle } from "lucide-react";
 import { sizes, variants } from "@/lib/variants";
+import Trend from "./components/trends";
+import { ErrorBoundary } from "react-error-boundary";
 
 export default async function Dashboard() {
-
   return (
     <main className="space-y-8">
-      {/* <section className="mb-8 grid grid-cols-2 lg:grid-cols-4 gap-8">
-        <Trend type="income" />
-        <Trend type="expense" />
-        <Trend type="saving" />
-        <Trend type="investment" />
-      </section> */}
+      <section className="mb-8 grid grid-cols-2 lg:grid-cols-4 gap-8">
+        <ErrorBoundary fallback={<p>Error loading trend data</p>}>
+          <Trend type="income" />
+        </ErrorBoundary>
+        <ErrorBoundary fallback={<p>Error loading trend data</p>}>
+          <Trend type="expense" />
+        </ErrorBoundary>
+        <ErrorBoundary fallback={<p>Error loading trend data</p>}>
+          <Trend type="saving" />
+        </ErrorBoundary>
+        <ErrorBoundary fallback={<p>Error loading trend data</p>}>
+          <Trend type="investment" />
+        </ErrorBoundary>
+      </section>
 
       <section className="flex justify-between items-center mb-8 sticky top-0 z-10 ">
         <h2 className="text-2xl">Transactions</h2>
