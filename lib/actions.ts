070c1d80fabeff46ea579b1d0fa7be6391eb45f2
Mahdi-Khorshidi-26 <khorshidi.mahdi.1998@gmail.com>
2025-09-13 01:23:10 +0330

    validation on the server .

--- lib/actions.ts
+++ lib/actions.ts
@@ -4,12 +4,18 @@ import { revalidatePath } from "next/cache";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { FieldValues } from "react-hook-form";
+import { transactionSchema } from "./validation";
 
 export async function addTransaction(data: FieldValues) {
+  const validated = transactionSchema.safeParse(data);
+  if (!validated.success) {
+    throw new Error(`Validation failed`);
+  }
+
   const cookieStore = cookies();
   const { error } = await createClient(cookieStore)
     .from("transactions")
-    .insert(data);
+    .insert(validated.data);
   if (error) {
     throw new Error("Failed to add transaction");
   }
