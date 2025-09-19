693a0d5f660469bb9ce3b752ab69d23b71ffc647
Mahdi-Khorshidi-26 <khorshidi.mahdi.1998@gmail.com>
2025-09-19 17:48:45 +0330

    loading for updated .

---
+++ app/dashboard/transaction/[id]/edit/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div className="text-center mt-20">
+      <h1 className="text-3xl font-bold mb-4">Transaction Not Found</h1>
+      <p className="text-gray-600">
+        The transaction you are looking for does not exist.
+      </p>
+    </div>
+  );
+}
