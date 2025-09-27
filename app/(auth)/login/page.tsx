42d2ab4bdaa3ea3a277992f47806b9741e58df96
Mahdi-Khorshidi-26 <khorshidi.mahdi.1998@gmail.com>
2025-09-27 23:12:43 +0330

    redirection from login page to dashboard once logged in

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,6 +1,16 @@
+import { createClient } from "@/utils/supabase/server";
 import LoginForm from "./components/loginForm";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function Login() {
+export default async function Login() {
+  const supabase = createClient(cookies());
+  const { data } = await supabase.auth.getUser();
+  const user = data.user;
+
+  if (user && user.id) {
+    return redirect("/dashboard");
+  }
   return (
     <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px] py-4">
       <div className="flex flex-col space-y-8 text-center">
