44b3e195b0bf915245d9a850969e2a01fc203506
mahdi-khorshidi-26 <mahdi.khorshidi.1998.ada@gmail.com>
2025-10-01 14:31:11 +0330

    promis fix

--- app/dashboard/transaction/[id]/edit/page.tsx
+++ app/dashboard/transaction/[id]/edit/page.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 export default async function EditTransactionPage({
   params,
 }: {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
   const cookieStore = cookies();
